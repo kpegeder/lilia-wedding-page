d5636593951e256e80774b9c4873a92cb1282ff5
kpegeder <k.pegeder@gmail.com>
2021-01-09 14:26:48 -0800

    add more wedding party photos

--- client/src/components/Friends/index.js
+++ client/src/components/Friends/index.js
@@ -3,6 +3,14 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import Calista from "../../assets/images/weddingParty/calista.png";
 import Liv from "../../assets/images/weddingParty/liv.png";
 import Kaari from "../../assets/images/weddingParty/kaari.png";
+import Myra from "../../assets/images/weddingParty/myra.png";
+import Nicole from "../../assets/images/weddingParty/nicole.png";
+import Jeffry from "../../assets/images/weddingParty/jeffry.png";
+import Seth from "../../assets/images/weddingParty/seth.png";
+import Erik from "../../assets/images/weddingParty/erik.png";
+import Ethan from "../../assets/images/weddingParty/ethan.png";
+import Daniel from "../../assets/images/weddingParty/daniel.png";
+import James from "../../assets/images/weddingParty/james.png";
 import "./style.css";
 
 const Friends = () => {
@@ -37,16 +45,72 @@ const Friends = () => {
           />
           <div className="partyList">Amanda Leslie</div>
           <div className="partyList">Nicole Bull</div>
+          <Image
+            className="portrait"
+            src={Nicole}
+            alt="Nicole - Bridesmaid"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">Myra Newingham</div>
+          <Image
+            className="portrait"
+            src={Myra}
+            alt="Myra - Bridesmaid"
+            rounded
+            loading="lazy"
+          />
         </Col>
         <Col xs={12} md={6}>
           <div className="headParty">Groomsman</div>
           <div className="partyList">Jeffry Davidson - Best Man</div>
+          <Image
+            className="portrait"
+            src={Jeffry}
+            alt="Jeffry - Groomsman"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">James Leslie</div>
+          <Image
+            className="portrait"
+            src={James}
+            alt="James - Groomsman"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">Seth Fehringer</div>
+          <Image
+            className="portrait"
+            src={Seth}
+            alt="Seth - Groomsman"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">Erik Gile</div>
+          <Image
+            className="portrait"
+            src={Erik}
+            alt="Erik - Groomsman"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">Ethan Pempek</div>
+          <Image
+            className="portrait"
+            src={Ethan}
+            alt="Ethan - Groomsman"
+            rounded
+            loading="lazy"
+          />
           <div className="partyList">Daniel Powell</div>
+          <Image
+            className="portrait"
+            src={Daniel}
+            alt="Daniel - Groomsman"
+            rounded
+            loading="lazy"
+          />
         </Col>
       </Row>
     </Container>
