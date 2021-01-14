0944f688d654c8b48c0dfd9206db7b74d7b5f3d5
kpegeder <k.pegeder@gmail.com>
2021-01-13 17:32:25 -0800

    add things to do

--- client/src/components/Activity/index.js
+++ client/src/components/Activity/index.js
@@ -6,12 +6,49 @@ const Activity = () => {
   return (
     <Container>
       <Row>
-        <Col className="activityDesc">
-          Yellowstone National Park- 4hr drive SE
-        </Col>
+        <Col className="parks">Parks to Visit</Col>
       </Row>
       <Row>
-        <Col className="activityDesc">Glacier Park- 2.5hr drive N</Col>
+        <Col className="activityDesc">
+          Yellowstone National Park - 4hr drive SE
+        </Col>
+        <Col className="activityDesc">Glacier Park - 2.5hr drive N</Col>
+        <Col className="activityDesc">Flathead Lake - 2hr drive NW</Col>
+      </Row>
+      <Row>
+        <Col className="parks">Websites to Checkout</Col>
+      </Row>
+      <Row>
+        <Col className="center">
+          <a
+            className="websites"
+            target="_blank"
+            rel="noreferrer"
+            href="http://www.lincolnmontana.com/"
+          >
+            Visit Lincoln, Montana
+          </a>
+        </Col>
+        <Col className="center">
+          <a
+            className="websites"
+            target="_blank"
+            rel="noreferrer"
+            href="https://southwestmt.com/communities/lincoln.htm"
+          >
+            Visit Southwest Montana
+          </a>
+        </Col>
+        <Col className="center">
+          <a
+            className="websites"
+            target="_blank"
+            rel="noreferrer"
+            href="https://continentaldividetrail.org/cdt-gateway-communities/lincoln/"
+          >
+            Continental Divide Trail
+          </a>
+        </Col>
       </Row>
     </Container>
   );
