8647f8b4d43a3e2418d514c6d35b1b99cf914a30
kpegeder <k.pegeder@gmail.com>
2021-01-15 21:53:06 -0800

    update registry border

--- client/src/components/Websites/index.js
+++ client/src/components/Websites/index.js
@@ -30,50 +30,6 @@ function Websites() {
           </a>
         </Col>
       </Row>
-      <Row>
-        <Col xs={12} sm={6} md={{ span: 4, offset: 2 }} className="registryWeb">
-          <a
-            href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/549776481"
-            target="_blank"
-            rel="noreferrer"
-            className="centerLogo"
-          >
-            <Image src={bedBath} fluid className="logo1" />
-          </a>
-        </Col>
-        <Col xs={12} sm={6} md={4} className="registryWeb">
-          <a
-            href="https://www.amazon.com/wedding/share/liliaanddave"
-            target="_blank"
-            rel="noreferrer"
-            className="centerLogo"
-          >
-            <Image src={amazon} fluid className="logo1 amazonLogo" />
-          </a>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} sm={6} md={{ span: 4, offset: 2 }} className="registryWeb">
-          <a
-            href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/549776481"
-            target="_blank"
-            rel="noreferrer"
-            className="centerLogo"
-          >
-            <Image src={bedBath} fluid className="logo2" />
-          </a>
-        </Col>
-        <Col xs={12} sm={6} md={4} className="registryWeb">
-          <a
-            href="https://www.amazon.com/wedding/share/liliaanddave"
-            target="_blank"
-            rel="noreferrer"
-            className="centerLogo"
-          >
-            <Image src={amazon} fluid className="logo2 amazonLogo" />
-          </a>
-        </Col>
-      </Row>
     </Container>
   );
 }
