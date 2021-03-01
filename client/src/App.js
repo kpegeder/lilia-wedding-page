d6223fd861c624ea0bc1436cbba2bb09c77c7513
kpegeder <k.pegeder@gmail.com>
2021-02-28 20:19:21 -0800

    update removed page

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Registry from "./pages/Registry";
 import RSVP from "./pages/RSVP";
 import Travel from "./pages/Travel";
 import ThingsToDo from "./pages/ThingsToDo";
-import Responses from "./pages/Responses";
+// import Responses from "./pages/Responses";
 import "./App.css";
 
 function App() {
@@ -26,7 +26,7 @@ function App() {
           <Route path="/thingstodo" component={ThingsToDo} />
           <Route path="/travel" component={Travel} />
           <Route path="/photos" component={Photos} />
-          <Route path="/responses" component={Responses} />
+          {/* <Route path="/responses" component={Responses} /> */}
           <Route path={["/", "/home"]} component={Home} />
         </Switch>
       </div>
